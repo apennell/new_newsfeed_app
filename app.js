e63a11609893a641b58ae75c70d59d4c4a58b6b5
Annie <annie@anniepennell.com>
2015-04-05 22:56:40 -0700

    works now

--- app.js
+++ app.js
@@ -10,46 +10,51 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Refactor connection and query code
 var db = require("./models");
 
+
 app.get('/articles', function(req,res) {
   db.Article.all().then(function(articles) {
     res.render('articles/index', {articlesList: articles});
   });
 });
 
+
 app.get('/articles/new', function(req,res) {
   res.render('articles/new');
 });
 
+
 app.post('/articles', function(req,res) {
-
-  console.log("\n\n\n\n\n\nTHIS IS ARTICLES", articles);
-
   db.Article.create({title: req.body.title, author: req.body.author, content: req.body.content})
   	.then(function(articles) {
   		res.redirect('/articles');
   	});
 });
 
+
 app.get('/articles/:id', function(req, res) {
-  var articleId = req.params.id;
-  
-  db.Article.find(articleId)
-  	.then(function(article) {
-  	  res.render('article', {articleToDisplay: article});
-  	});
+ var articleId = req.params.id;
+
+ db.Article.find(articleId)
+     .then(function(article) {
+       res.render('articles/article', {articleToDisplay: article});
+     });
 });
+
 
 app.get('/', function(req,res) {
   res.render('site/index.ejs');
 });
 
+
 app.get('/about', function(req,res) {
   res.render('site/about');
 });
 
+
 app.get('/contact', function(req,res) {
   res.render('site/contact');
 });
+
 
 app.listen(3000, function() {
   console.log('Listening');
